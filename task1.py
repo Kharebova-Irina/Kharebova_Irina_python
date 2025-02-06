2af707f81f7354b777133bd80c22f4f2fb06077d
Kharebova-Irina <89922479+Kharebova-Irina@users.noreply.github.com>
2025-02-06 23:14:05 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,11 @@
 import numpy as np
 numbers=int(input('Введи число: '))
-step=int(input('Введи шаг: '))
+step=1
 list_1=[]
 for i in range(1, numbers+1):
     list_1.append(i)
 k=len(list_1)
-a=np.zeros(30)
+a=np.zeros(3*len(list_1))
 
 start=1
 end=start+step
@@ -14,6 +14,8 @@ a[0]=list_1[0]
 while i!=len(a)-1:
     for i in range(start, end, step):
         a[i]=list_1[i%k]
+        print(a)
+
     start=end
     end=start+step
 list_2=[]
