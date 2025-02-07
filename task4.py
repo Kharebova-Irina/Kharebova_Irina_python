7ec16a8d01df673e8a5e6db42a5e33077506e310
Kharebova-Irina <89922479+Kharebova-Irina@users.noreply.github.com>
2025-02-07 12:07:25 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,16 +1,36 @@
 import numpy as np
+import sys
 
-a=map(int, input().split())
-a=list(a)
-cnt=0
-for i in range(len(a)):
-    while a[i]!=a[1]:
-        if a[i]<a[1]:
-            a[i]+=1
-            cnt+=1
-        elif a[i]==a[1]:
-            i+=1
-        elif a[i]>a[1]:
-            a[i]-=1
-            cnt+=1
-print(cnt)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Введи числа через пробел")
+        return
+
+    try:
+        a = list(map(int, sys.argv[1:]))
+    except ValueError:
+        print("Ошибка: все аргументы должны быть целыми числами")
+        return
+
+    if len(a) < 2:
+        print("Ошибка: требуется минимум 2 числа")
+        return
+
+    cnt = 0
+    min = a[0]
+
+    for i in range(len(a)):
+        while a[i] != min:
+            if a[i] < min:
+                a[i] += 1
+                cnt += 1
+            elif a[i] > min:
+                a[i] -= 1
+                cnt += 1
+
+    print(cnt)
+
+
+if __name__ == "__main__":
+    main()
